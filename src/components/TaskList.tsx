3be8eb3c7c474def379829096d7733dc19c030bd
Gustavo Barbieri <gustavobarbieri@ymail.com>
2021-03-10 22:43:45 -0300

    Working with states

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+      setTasks([...tasks, {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }])
+    };
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTasks = tasks.map(t => t.id === id ? {
+      ...t,
+      isComplete: !t.isComplete
+    } : t);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(t => t.id !== id);
+    setTasks(filteredTask);
   }
 
   return (
